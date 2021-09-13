10ca43bdb5a9665b46ed249ab24643de1ceaf162
Martin Steven <martinstv2019@gmail.com>
2021-09-12 21:27:41 -0500

    Diseño-Timer__v2
    Optimización de código , tomates agregados y animación del timer.

---
+++ Primer_Sprint/Steven/Pag__v1/js/animations_timer.js
@@ -0,0 +1,45 @@
+const userName = document.querySelector("#username")
+const btnUser = document.querySelector(".icon-down");
+const div = document.querySelector(".timer--opacity");
+const firstDiv = document.querySelector(".no--opacity");
+const container1 = document.querySelector("#container-1");
+const container2 = document.querySelector("#container-2");
+const firstNumber = document.querySelector("#first-number");
+const lastNumber = document.querySelector("#last-number");
+
+/*ANIMACION USUARIO*/
+userName.addEventListener("click" , e=>{
+    e.preventDefault();
+    if(btnUser.classList.contains("icon-down")){
+        btnUser.classList.remove("icon-down")
+        btnUser.classList.add("icon-transform");
+    }else{
+        btnUser.classList.remove("icon-transform")
+        btnUser.classList.add("icon-down");
+    }    
+})
+
+/*ANIMACION TIMER */
+const iconTime = document.querySelectorAll(".time-icon");
+    iconTime.forEach(icon =>{
+        icon.addEventListener("click" , () =>{
+            container1.classList.toggle("order1");
+            if(container1.classList.contains("disabled")){
+                container1.classList.remove("disabled");
+                container1.classList.remove("timer-container--max-width");
+                lastNumber.classList.remove("first-time");
+                container2.classList.add("disabled");
+                firstNumber.classList.add("first-time");
+                container2.classList.add("timer-container--max-width");
+            }else{
+                firstNumber.classList.remove("first-time");
+                container2.classList.remove("disabled")
+                container2.classList.remove("timer-container--max-width");
+                container1.classList.add("disabled");
+                container1.classList.add("timer-container--max-width");
+                lastNumber.classList.add("first-time");
+            }
+        })
+    })
+    
+
